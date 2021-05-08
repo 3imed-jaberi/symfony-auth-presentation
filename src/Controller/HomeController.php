a9dd6dc8cda8a7b042070dcc4b0da1c8a339e5de
3imed-jaberi <imed_jebari@hotmail.fr>
2021-05-08 05:50:18 +0200

    add navbar + fix home ctrl 🥽 ..

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,22 +2,18 @@
 
 namespace App\Controller;
 
-use App\Entity\Article;
-use App\Form\ArticleType;
-use App\Repository\ArticleRepository;
-use Symfony\Component\HttpFoundation\Request;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\String\Slugger\SluggerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class BlogController extends AbstractController
+class HomeController extends AbstractController
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/admin", name="homepage")
      */
     public function index(): Response
     {
-        return $this->render('blog/index.html.twig');
+        return $this->render('home/home.html.twig');
     }
 }
